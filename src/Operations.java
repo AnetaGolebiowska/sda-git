82e001415c1e22edfcf86d25eb08982ea94d98a0
AnetaGolebiowska <aneta.golebiowskaa@gmail.com>
2019-03-02 15:52:05 +0100

    Komentarz

--- src/Operations.java
+++ src/Operations.java
@@ -1,5 +1,18 @@
 public class Operations {
-    public static int giveAnswer() {
-        throw new NoSuchMethodError();
+    public static int giveAnswer(String działanie, int liczba1, int liczba2) {
+        char znak = działanie.charAt(0);
+
+        if (znak == '+') {
+            return liczba1 + liczba2;
+        } else if (znak == '-') {
+            return liczba1 - liczba2;
+        } else if (znak == '*') {
+            return liczba1 * liczba2;
+        } else if (znak == '/'){
+            return liczba1 / liczba2;
+        } else{
+            throw new UnsupportedOperationException(
+                    "To nie jest znak działania matematycznego");
+        }
     }
 }
