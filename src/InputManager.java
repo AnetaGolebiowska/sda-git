d211c3e6d4789bd44d1fe4edb3293c8bc6685567
Your Name <you@example.com>
2019-03-02 15:44:51 +0100

    number validator and input menager

--- src/InputManager.java
+++ src/InputManager.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class InputManager {
+
+
     public static String getOperation() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj znak działania: *,+,-,/");
+        String dzialanie = scanner.next();
         throw new NoSuchMethodError();
     }
 
     public static String getNumber() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj 2 liczby. Podaj 1 liczbe:");
+        String liczba1 = scanner.next();
+        System.out.println("Podaj 2 liczbe");
+        String liczba2 = scanner.next();
+
         throw new NoSuchMethodError();
     }
 }
--- src/NumberValidator.java
+++ src/NumberValidator.java
@@ -1,5 +1,13 @@
-public class NumberValidator {
-    public static void validate(String liczba1) {
-        throw new NoSuchMethodError();
+
+public class NumberValidator
+
+{
+    public static void validate(String liczba1)
+
+    {
+       for(char ch: liczba1.toCharArray() )
+        if (Character.isDigit(ch));
+        else{
+        throw new NoSuchMethodError();}
     }
 }
